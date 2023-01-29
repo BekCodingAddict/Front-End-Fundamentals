f1a74b56bf718c4f57001abe64cf1cf923f374cc
BekCodingAddict <experte001@gmail.com>
2023-01-29 23:50:35 +0900

    HTML with JavaScript!

---
+++ HTML/JS/HTML_with_JavaScript.js
@@ -0,0 +1,5 @@
+document.getElementById('demo1').innerHTML="Hello JavaScript!";
+document.getElementById('demo1').style.fontSize="16px";
+document.getElementById('demo1').style.color='Green';
+document.getElementById('demo1').style.backgroundColor="yellow";
+document.getElementById('demo1').style.padding="20px";
