cb8638c46fbc39f4acca253dba4ccc95369ae32b
BekCodingAddict <experte001@gmail.com>
2023-07-27 17:59:13 +0900

    JavaScript Math!

---
+++ JavaScript/JS/26.JS-Math.js
@@ -0,0 +1,48 @@
+///Math Properties (Constants)
+
+console.log('Math.Pi:'+Math.PI);
+console.log('Math.Pi:'+Math.E);
+console.log('Math.Pi:'+Math.SQRT1_2);
+console.log('Math.Pi:'+Math.LN10);
+
+//Math Methods
+console.log('Math.round:'+Math.round(4.6));
+console.log('Math.round:'+Math.round(4.5));
+console.log('Math.round:'+Math.round(4.4));
+
+//Math.ceil(x) returns the value of x rounded up to its nearest integer:
+
+console.log('Math.ceil:'+Math.ceil(4.9));
+console.log('Math.ceil:'+Math.ceil(4.7));
+console.log('Math.ceil:'+Math.ceil(4.4));
+console.log('Math.ceil:'+Math.ceil(4.2));
+console.log('Math.ceil:'+Math.ceil(-4.2));
+
+//Math.floor(x) returns the value of x rounded down to its nearest integer:
+console.log('Math.floor:'+Math.floor(4.9));
+console.log('Math.floor:'+Math.floor(4.7));
+console.log('Math.floor:'+Math.floor(4.4));
+console.log('Math.floor:'+Math.floor(4.2));
+console.log('Math.floor:'+Math.floor(-4.2));
+
+//Math.trunc(x) returns the integer part of x:
+console.log('Math.trunc:'+Math.trunc(4.9));
+console.log('Math.trunc:'+Math.trunc(4.7));
+console.log('Math.trunc:'+Math.trunc(4.4));
+console.log('Math.trunc:'+Math.trunc(4.2));
+console.log('Math.trunc:'+Math.trunc(-4.2));
+
+//Math.sign(x) returns if x is negative, null or positive:
+console.log('Math.sign:'+Math.sign(-4));
+console.log('Math.sign:'+Math.sign(0));
+console.log('Math.sign:'+Math.sign(4));
+
+//Math.pow()
+console.log("Math.pow:"+Math.pow(3,3));
+
+//Math.sqrt()
+console.log('Math.sqrt:'+Math.sqrt(64));
+
+//Math.abs()
+console.log('Math.abs:'+Math.abs(-4.7));
+console.log('Math.abs:'+Math.abs(-0.5));
