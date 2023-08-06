9fa70b2b5c515335e857e19a33b8aa2994f2f332
BekCodingAddict <experte001@gmail.com>
2023-08-06 18:18:05 +0900

    JavaScript If Else

---
+++ JavaScript/JS/30.JS-If-Else.js
@@ -0,0 +1,10 @@
+//JavaScript if, else, and else if
+
+let hour=9;
+if(hour<=9){
+    console.log("Good Morning!");
+}else if(hour>=12 && hour<=17){
+    console.log('Good Afternoon!');
+}else{
+    console.log('Good Evening!');
+}
