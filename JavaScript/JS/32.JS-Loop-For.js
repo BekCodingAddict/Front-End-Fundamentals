6a5addd65d0c8c03a69560de2c562ab9ee124017
BekCodingAddict <experte001@gmail.com>
2023-08-10 21:56:02 +0900

    JavaScript for in loop!

--- JavaScript/JS/32.JS-Loop-For.js
+++ JavaScript/JS/32.JS-Loop-For.js
@@ -10,3 +10,31 @@ for (let i=0;i<car.length;i++){
 for (let i=0, len=car.length,text="Hello?";i<len;i++){
     console.log('len='+len+" text="+text);
 }
+
+//The For In Loop
+const person={fname:'Jaloli',lname:'Jumanov',age:26};
+let txt="";
+for (let x in person){
+    console.log('person.'+x+'='+person[x]+';');
+    txt+=person[x]+'';
+}
+console.log('txt='+txt);
+
+//The JavaScript for in statement can also loop over the properties of an Array:
+const numbers=[45,4,9,16,25];
+let text="";
+for(let x in numbers){
+    text+=numbers[x];
+    text+=',';
+
+}
+
+console.log('text:'+text);
+
+let txt2="";
+numbers.forEach(myfunction);
+function myfunction(value,index,array){
+    txt2+=value;
+    txt2+=",";
+}
+console.log('txt2='+txt2);
