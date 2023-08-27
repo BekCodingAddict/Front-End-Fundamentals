30b58bb1551f80b8bfb74a2129332701614d2efd
BekCodingAddict <experte001@gmail.com>
2023-08-27 16:27:47 +0900

    JavaScript Best Practices!

---
+++ JavaScript/JS/53.JS-Style-Guide.js
@@ -0,0 +1,36 @@
+//JavaScript Style Guide
+
+fristName="John";
+lastName='Doe';
+
+price=19.90;
+tax=0.20;
+
+fullPrice=price+(price*tax);
+
+//Statement Rules
+const cars = ["Volvo", "Saab", "Fiat"];
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
+
+// Functions:
+function toCelsius(fahrenheit) {
+  return (5 / 9) * (fahrenheit - 32);
+}
+
+// Loops:
+for (let i = 0; i < 5; i++) {
+  x += i;
+}
+
+// Conditionals:
+if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
