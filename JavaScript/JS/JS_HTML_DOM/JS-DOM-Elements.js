08b2a88c66678961af264734e4fdd8db266f605d
BekCodingAddict <experte001@gmail.com>
2023-10-20 14:48:08 +0900

    JavaScript HTML DOM elements and html!

---
+++ JavaScript/JS/JS_HTML_DOM/JS-DOM-Elements.js
@@ -0,0 +1,7 @@
+
+function demo2(){
+    let switcher=0;
+    switcher===0?document.getElementById('demo2').src='https://th.bing.com/th/id/OIP.hZ5gTa9OtpTpJK-DKIOpywHaEc?pid=ImgDet&rs=1':switcher=1;
+}
+
+document.getElementById('demo3').innerHTML=Date();
